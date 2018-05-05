62f8b0f0d88e2c78b0b61c6f4e4b8178bc3ecc8b
badman27 <30854558+badman27@users.noreply.github.com>
2018-05-05 00:14:46 -0700

    Initial Commit. Add Camera and Photo Gallery Functionality.

---
+++ WhatFlower/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  WhatFlower
+//
+//  Created by Neema Badihian on 5/4/18.
+//  Copyright © 2018 Neema Badihian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = false
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+    }
+
+}
+
